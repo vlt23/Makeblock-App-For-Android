vlt23/Makeblock-App-For-Android

commit 02bd396bb8b67ebdc45a771d6416bde6e1017df4

CarController: replace 8 buttons to one joystick
[TODO] There has a bug when release the pointer, sometimes the button position is not reset for a unknown reason...

app/src/main/java/cc/makeblock/modules/MeCarController.java
@@ -1,201 +1,118 @@
 package cc.makeblock.modules;
 
-import android.annotation.SuppressLint;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import org.json.JSONObject;
-
 import cc.makeblock.makeblock.MeDevice;
 import cc.makeblock.makeblock.R;
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+import org.json.JSONObject;
 
 public class MeCarController extends MeModule {
     static String devName = "carcontroller";
-    private ImageButton mLeftUpButton;
-    private ImageButton mLeftDownButton;
-    private ImageButton mRightUpButton;
-    private ImageButton mRightDownButton;
-    private ImageButton mLeftButton;
-    private ImageButton mDownButton;
-    private ImageButton mRightButton;
-    private ImageButton mUpButton;
-    private ImageButton mSpeedButton;
-    private TextView mSpeedLabel;
-    private int motorSpeed = 180;
+
+    private JoystickView joystickView;
+
     private final Handler mStopHandler = new Handler(Looper.getMainLooper());
-    private final Runnable mStopRunnable = new Runnable() {
-        @Override
-        public void run() {
-            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, 0, 0);
-            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-        }
+    private final Runnable mStopRunnable = () -> {
+        byte[] wr = buildJoystickWrite(DEV_JOYSTICK, 0, 0);
+        mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
     };
+
+    private boolean isPreviousStopped;  // Prevent loop sending 0 for leftSpeed and rightSpeed
 
     public MeCarController(int port, int slot) {
         super(devName, MeModule.DEV_DCMOTOR, port, slot);
+        initRestComponents();
+    }
+
+    public MeCarController(JSONObject jObj) {
+        super(jObj);
+        initRestComponents();
+    }
+
+    private void initRestComponents() {
         viewLayout = R.layout.dev_car_controller;
         this.scale = 1.33f;
     }
 
-    public MeCarController(JSONObject jobj) {
-        super(jobj);
-        viewLayout = R.layout.dev_car_controller;
-        this.scale = 1.33f;
-    }
-
-    long ctime = System.currentTimeMillis();
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void setEnable(Handler handler) {
         mHandler = handler;
-        findAllViewById();
-        mLeftUpButton.setClickable(true);
-        mRightUpButton.setClickable(true);
-        mLeftDownButton.setClickable(true);
-        mRightDownButton.setClickable(true);
-        mLeftUpButton.setEnabled(true);
-        mRightUpButton.setEnabled(true);
-        mLeftDownButton.setEnabled(true);
-        mRightDownButton.setEnabled(true);
-        View.OnTouchListener touchListener = new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent evt) {
-                v.performClick();
-                if (evt.getAction() == MotionEvent.ACTION_UP) {
-                    MeDevice.sharedManager().manualMode = false;
-                    byte[] wr = buildJoystickWrite(DEV_JOYSTICK, 0, 0);
-                    mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                    mStopHandler.postDelayed(mStopRunnable, 150);
-                    return true;
-                } else if (evt.getAction() == MotionEvent.ACTION_DOWN) {
-                    if (System.currentTimeMillis() - ctime > 80) {
-                        ctime = System.currentTimeMillis();
-                        MeDevice.sharedManager().manualMode = true;
-                        if (v.equals(mLeftUpButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, motorSpeed / 2, motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mLeftDownButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, -motorSpeed / 2, -motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mRightUpButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, motorSpeed, motorSpeed / 2);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mRightDownButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, -motorSpeed, -motorSpeed / 2);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mLeftButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, -motorSpeed, motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mRightButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, motorSpeed, -motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mUpButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, motorSpeed, motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        } else if (v.equals(mDownButton)) {
-                            byte[] wr = buildJoystickWrite(DEV_JOYSTICK, -motorSpeed, -motorSpeed);
-                            mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
-                        }
-                    }
-                }
-                return false;
+        joystickView = view.findViewById(R.id.joystickCar);
+        joystickView.setEnabled(true);
+        joystickView.setFixedCenter(true);  // set fix (0, 0) center
+        joystickView.setAutoReCenterButton(true);
+
+        View.OnTouchListener touchListener = (v, evt) -> {
+            v.performClick();
+            int action = evt.getAction();
+            if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_CANCEL) {
+                // Reset the joystick to the center
+                joystickView.resetButtonPosition();
+                stop();
+                return true;
             }
+            return false;
         };
-        mLeftButton.setClickable(true);
-        mRightButton.setClickable(true);
-        mUpButton.setClickable(true);
-        mDownButton.setClickable(true);
-        mLeftButton.setEnabled(true);
-        mRightButton.setEnabled(true);
-        mUpButton.setEnabled(true);
-        mDownButton.setEnabled(true);
 
-        mLeftButton.setOnTouchListener(touchListener);
-        mRightButton.setOnTouchListener(touchListener);
-        mUpButton.setOnTouchListener(touchListener);
-        mDownButton.setOnTouchListener(touchListener);
-        mLeftUpButton.setOnTouchListener(touchListener);
-        mRightUpButton.setOnTouchListener(touchListener);
-        mLeftDownButton.setOnTouchListener(touchListener);
-        mRightDownButton.setOnTouchListener(touchListener);
+        joystickView.setOnTouchListener(touchListener);
 
-        mSpeedButton.setClickable(true);
-        mSpeedButton.setEnabled(true);
-        mSpeedButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent evt) {
-                v.performClick();
-                int y = (int) evt.getY();
-                //Log.d("mb", ""+x+":"+y);
-                if (y < 48) {
-                    motorSpeed += 4;
-                } else {
-                    motorSpeed -= 4;
+        joystickView.setOnMoveListener((angle, strength) -> {
+            Log.d(devName, "angle: " + angle + ", strength: " + strength);
+            if (strength < 10) {
+                if (!isPreviousStopped) {
+                    stop();
                 }
-
-                motorSpeed = motorSpeed > 255 ? 255 : (Math.max(motorSpeed, 0));
-                MeDevice.sharedManager().motorSpeed = motorSpeed;
-                mSpeedLabel = view.findViewById(R.id.speedLabel);
-                mSpeedLabel.setText("Speed:" + motorSpeed);
-                return false;
+            } else {
+                int speed = (int) (strength * 1.2);
+                int leftSpeed, rightSpeed;
+                // Linear Interpolator: Y = ( ( X - X1 )( Y2 - Y1) / ( X2 - X1) ) + Y1
+                if (angle < 90) {
+                    rightSpeed = speed;
+                    float linearInterpolator = ((float) angle - 0) * (1 - (-1)) / (90 - 0) - 1;
+                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    leftSpeed = (int) (linearInterpolator * speed);
+                } else if (angle < 180) {
+                    leftSpeed = -speed;
+                    float linearInterpolator = ((float) angle - 90) * (1 - (-1)) / (180 - 90) - 1;
+                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    rightSpeed = (int) (linearInterpolator * speed);
+                } else if (angle < 270) {
+                    leftSpeed = speed;
+                    float linearInterpolator = ((float) angle - 180) * (-1 - 1) / (270 - 180) + 1;
+                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    rightSpeed = (int) (linearInterpolator * speed);
+                } else {
+                    rightSpeed = -speed;
+                    float linearInterpolator = ((float) angle - 270) * (-1 - 1) / (360 - 270) + 1;
+                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    leftSpeed = (int) (linearInterpolator * speed);
+                }
+                Log.d(devName, "leftSpeed: " + leftSpeed + ", rightSpeed: " + rightSpeed);
+                isPreviousStopped = false;
+                byte[] wr = buildJoystickWrite(DEV_JOYSTICK, leftSpeed, rightSpeed);
+                mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
             }
-        });
-        mSpeedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Log.d("mb", "speed");
-            }
-        });
-    }
-
-    private void findAllViewById() {
-        mLeftUpButton = view.findViewById(R.id.leftUpButton);
-        mRightUpButton = view.findViewById(R.id.rightUpButton);
-        mLeftDownButton = view.findViewById(R.id.leftDownButton);
-        mRightDownButton = view.findViewById(R.id.rightDownButton);
-        mLeftButton = view.findViewById(R.id.leftButton);
-        mRightButton = view.findViewById(R.id.rightButton);
-        mUpButton = view.findViewById(R.id.upButton);
-        mDownButton = view.findViewById(R.id.downButton);
-        mSpeedButton = view.findViewById(R.id.speedButton);
+        }, 100);
     }
 
     @Override
     public void setDisable() {
-        findAllViewById();
-        mLeftUpButton.setOnClickListener(null);
-        mRightUpButton.setOnClickListener(null);
-        mLeftDownButton.setOnClickListener(null);
-        mRightDownButton.setOnClickListener(null);
-        mLeftButton.setOnClickListener(null);
-        mRightButton.setOnClickListener(null);
-        mUpButton.setOnClickListener(null);
-        mDownButton.setOnClickListener(null);
-        mSpeedButton.setOnClickListener(null);
-        mLeftUpButton.setClickable(false);
-        mRightUpButton.setClickable(false);
-        mLeftDownButton.setClickable(false);
-        mRightDownButton.setClickable(false);
-        mLeftUpButton.setEnabled(false);
-        mRightUpButton.setEnabled(false);
-        mLeftDownButton.setEnabled(false);
-        mRightDownButton.setEnabled(false);
-        mLeftButton.setClickable(false);
-        mRightButton.setClickable(false);
-        mUpButton.setClickable(false);
-        mDownButton.setClickable(false);
-        mLeftButton.setEnabled(false);
-        mRightButton.setEnabled(false);
-        mUpButton.setEnabled(false);
-        mDownButton.setEnabled(false);
-        mSpeedButton.setClickable(false);
-        mSpeedButton.setEnabled(false);
+        joystickView = view.findViewById(R.id.joystickCar);
+        joystickView.setOnTouchListener(null);
+        joystickView.setOnMoveListener(null);
+        joystickView.setEnabled(false);
+    }
+
+    private void stop() {
+        MeDevice.sharedManager().manualMode = false;
+        byte[] wr = buildJoystickWrite(DEV_JOYSTICK, 0, 0);
+        mHandler.obtainMessage(MSG_VALUE_CHANGED, wr).sendToTarget();
+        mStopHandler.postDelayed(mStopRunnable, 150);
+        isPreviousStopped = true;
     }
 
 }

app/build.gradle
@@ -33,4 +33,5 @@ android {
 
 dependencies {
     implementation "androidx.appcompat:appcompat:1.2.0"
+    implementation 'io.github.controlwear:virtualjoystick:1.10.1'
 }

app/src/main/res/layout-hdpi/dev_car_controller.xml
@@ -4,115 +4,15 @@
     android:layout_height="match_parent"
     android:orientation="vertical">
 
-    <FrameLayout
-        android:layout_width="fill_parent"
-        android:layout_height="wrap_content">
-
-        <TextView
-            android:id="@+id/speedLabel"
-            android:layout_width="fill_parent"
-            android:layout_height="wrap_content"
-            android:layout_gravity="center"
-            android:gravity="center"
-            android:paddingBottom="5dp"
-            android:text="Speed:150" />
-
-    </FrameLayout>
-
-    <FrameLayout
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content">
-
-        <ImageButton
-            android:id="@+id/upButton"
-            android:layout_width="76px"
-            android:layout_height="96px"
-            android:layout_marginLeft="100px"
-            android:layout_marginTop="4px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/up_dir" />
-
-        <ImageButton
-            android:id="@+id/downButton"
-            android:layout_width="76px"
-            android:layout_height="96px"
-            android:layout_marginLeft="100px"
-            android:layout_marginTop="174px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/down_dir" />
-
-        <ImageButton
-            android:id="@+id/leftButton"
-            android:layout_width="96px"
-            android:layout_height="76px"
-            android:layout_marginLeft="4px"
-            android:layout_marginTop="100px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/left_dir" />
-
-        <ImageButton
-            android:id="@+id/rightButton"
-            android:layout_width="96px"
-            android:layout_height="76px"
-            android:layout_marginLeft="174px"
-            android:layout_marginTop="100px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/right_dir" />
-
-        <ImageButton
-            android:id="@+id/leftUpButton"
-            android:layout_width="82px"
-            android:layout_height="82px"
-            android:layout_marginLeft="9px"
-            android:layout_marginTop="9px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/leftup_dir" />
-
-        <ImageButton
-            android:id="@+id/rightUpButton"
-            android:layout_width="82px"
-            android:layout_height="82px"
-            android:layout_marginLeft="186px"
-            android:layout_marginTop="9px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/rightup_dir" />
-
-        <ImageButton
-            android:id="@+id/leftDownButton"
-            android:layout_width="82px"
-            android:layout_height="82px"
-            android:layout_marginLeft="9px"
-            android:layout_marginTop="186px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/leftdown_dir" />
-
-        <ImageButton
-            android:id="@+id/rightDownButton"
-            android:layout_width="82px"
-            android:layout_height="82px"
-            android:layout_marginLeft="186px"
-            android:layout_marginTop="186px"
-            android:background="#00000000"
-            android:scaleType="fitCenter"
-            android:src="@drawable/rightdown_dir" />
-
-        <ImageButton
-            android:id="@+id/speedButton"
-            android:layout_width="96px"
-            android:layout_height="96px"
-            android:layout_marginLeft="89px"
-            android:layout_marginTop="89px"
-            android:background="#00000000"
-            android:clickable="false"
-            android:scaleType="fitCenter"
-            android:src="@drawable/speed" />
-    </FrameLayout>
+    <io.github.controlwear.virtual.joystick.android.JoystickView xmlns:custom="http://schemas.android.com/apk/res-auto"
+        android:id="@+id/joystickCar"
+        android:layout_width="90dp"
+        android:layout_height="90dp"
+        custom:JV_buttonColor="#FF6E40"
+        custom:JV_buttonSizeRatio="10%"
+        custom:JV_borderColor="#00796B"
+        custom:JV_backgroundColor="#009688"
+        custom:JV_backgroundSizeRatio="85%"
+        custom:JV_borderWidth="3dp" />
 
 </LinearLayout>

commit 94177fbd8908a4d192cf16d2f91035e17b6ad78e

Fix the wrong behavior introduced by the previous 02bd396 commit

app/src/main/java/cc/makeblock/modules/MeCarController.java
@@ -61,36 +61,33 @@ public class MeCarController extends MeModule {
         joystickView.setOnTouchListener(touchListener);
 
         joystickView.setOnMoveListener((angle, strength) -> {
-            Log.d(devName, "angle: " + angle + ", strength: " + strength);
             if (strength < 10) {
                 if (!isPreviousStopped) {
                     stop();
                 }
             } else {
+                Log.d(devName, "angle: " + angle + ", strength: " + strength);
                 int speed = (int) (strength * 1.2);
                 int leftSpeed, rightSpeed;
-                // Linear Interpolator: Y = ( ( X - X1 )( Y2 - Y1) / ( X2 - X1) ) + Y1
+                float linearInterpolator;  // Y = ( ( X - X1 )( Y2 - Y1) / ( X2 - X1) ) + Y1
                 if (angle < 90) {
-                    rightSpeed = speed;
-                    float linearInterpolator = ((float) angle - 0) * (1 - (-1)) / (90) - 1;
-                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    linearInterpolator = ((float) angle - 0) * (-1 - 1) / (90) + 1;
                     leftSpeed = (int) (linearInterpolator * speed);
+                    rightSpeed = speed;
                 } else if (angle < 180) {
                     leftSpeed = -speed;
-                    float linearInterpolator = ((float) angle - 90) * (1 - (-1)) / (180 - 90) - 1;
-                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    linearInterpolator = ((float) angle - 90) * (-1 - 1) / (180 - 90) + 1;
                     rightSpeed = (int) (linearInterpolator * speed);
                 } else if (angle < 270) {
-                    leftSpeed = speed;
-                    float linearInterpolator = ((float) angle - 180) * (-1 - 1) / (270 - 180) + 1;
-                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
-                    rightSpeed = (int) (linearInterpolator * speed);
-                } else {
-                    rightSpeed = -speed;
-                    float linearInterpolator = ((float) angle - 270) * (-1 - 1) / (360 - 270) + 1;
-                    Log.d(devName, "linearInterpolator: " + linearInterpolator);
+                    linearInterpolator = ((float) angle - 180) * (1 - (-1)) / (270 - 180) - 1;
                     leftSpeed = (int) (linearInterpolator * speed);
+                    rightSpeed = -speed;
+                } else {
+                    leftSpeed = speed;
+                    linearInterpolator = ((float) angle - 270) * (1 - (-1)) / (360 - 270) - 1;
+                    rightSpeed = (int) (linearInterpolator * speed);
                 }
+                Log.d(devName, "linearInterpolator: " + linearInterpolator);
                 Log.d(devName, "leftSpeed: " + leftSpeed + ", rightSpeed: " + rightSpeed);
                 isPreviousStopped = false;
                 byte[] wr = buildJoystickWrite(DEV_JOYSTICK, leftSpeed, rightSpeed);